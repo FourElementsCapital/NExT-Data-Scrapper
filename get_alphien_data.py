acf08f9ded9b900f823a77178c3b2e45f3989f7c
yongcheng <yongcheng@4ecappcsg22.fourelements.sg>
2019-07-31 18:35:39 +0800

    added doc

--- get_alphien_data.py
+++ get_alphien_data.py
@@ -30,10 +30,24 @@ def m2ar(matrix,lag = False):
     return time_series
 
 def get_alphien_data(start_date, end_date, target_column):
+    '''
+        Get positioning data from alphien
+        Input
+        start_date(date)		: start date
+        end_date(date)			: end date
+        target_column(str)      : 
+        Output
+        time_series(df)		: Pandas DataFrame
+        Example
+            start_date = date(2014, 7, 28)
+            end_date = date(2018, 2, 23)
+            target_column = 'avg_net_norm'
+            get_alphien_data(start_date = start_date, end_date = end_date, target_column = target_column)
+    '''
     # Creating proper dataframe using R
     robjects.r('''tickers = getTickersPositions(exchange = "LME",
-                                direction = c("Net","Total"),
-                                account = c("Managed Money","Open Interest"))''')
+                                                direction = c("Net","Total"),
+                                                account = c("Managed Money","Open Interest"))''')
     robjects.r('''name = getReferenceBuildingBlock(fieldName = "name",
                                                    key = "researchTicker",selectionKey = tickers$commo)''')
     robjects.r('''colnam = tolower(paste0(name[,1],"_",tickers$direction))''')
