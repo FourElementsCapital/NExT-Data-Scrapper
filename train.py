bb43539ad790e6e02a0e7dd4054f32eb478c05bf
yongcheng4ecap <51808063+yongcheng4ecap@users.noreply.github.com>
2019-08-05 12:36:22 +0800

    Update train.py

--- train.py
+++ train.py
@@ -19,6 +19,11 @@ import utils
 import model
 from datetime import date, timedelta
 import numpy as np
+import rpy2.robjects as robjects
+robjects.r('.sourceQlib()')
+from rpy2.robjects.packages import importr
+rbase = importr('base')
+rzoo = importr('zoo')
 
 s = utils.SpacyHelper()
 db = utils.DatabaseHelper()
@@ -221,15 +226,60 @@ def train_model(authors, articles, positions, target_column, delta_days):
     return best_result, best_params
 
 
-def get_alphien_data(start_date, end_date):
-    date_range = pd.date_range(start_date, end_date)
+def m2ar(matrix,lag = False):
+    '''
+        convert from rmatrix to pandas DataFrame (4E server only)
+        Input
+        matrix(rmatrix)		: rmatrix that holds data with index of date
+        lag(bool)			: Boolean to decide whether lagging is required 
+        Output
+        time_series(df)		: Pandas DataFrame
+    '''
+    arr = np.array(matrix)
+    '''Get index'''
+    idx = rbase.as_character(rzoo.index(matrix))
+    '''Convert to pandas dataframe'''
+    if not lag:
+        time_series = pd.DataFrame(arr,index=idx)
+    else:
+        time_series = pd.DataFrame(arr[:-1],index = idx[1:])
+    '''Assign proper column names'''
+    time_series.columns = matrix.colnames
+    return time_series
 
-    data = {'article_date': date_range}
-
-    for k,v in mapper.items():
-        data[k] = np.random.randint(10000, 200000, len(date_range))
-
-    return pd.DataFrame(data)
+def get_alphien_data(start_date = None, end_date = None):
+    '''
+        Get positioning data from alphien
+        Input
+        start_date(date)		: start date
+        end_date(date)			: end date
+        target_column(str)      : 
+        Output
+        time_series(df)		: Pandas DataFrame
+        Example
+        start_date = date(2014, 7, 28)
+        end_date = date(2018, 2, 23)
+        get_alphien_data(start_date = start_date, end_date = end_date)
+    '''
+    # Creating proper dataframe using R
+    if start_date is None:
+        start_date = 'NULL'
+    else:
+        start_date = '"'+str(start_date)+'"'
+    if end_date is None:
+        end_date = 'NULL'
+    else:
+        end_date = '"'+str(end_date)+'"'
+    robjects.r('''tickers = getTickersPositions(exchange = "LME",
+                                                direction = c("Net","Total"),
+                                                account = c("Managed Money","Open Interest"))''')
+    robjects.r('''data = getSecurity(tickers$ticker,extension="Index",
+                                     start='''+start_date+''',end='''+end_date+''',
+                                     )''')
+    data = robjects.r('''data''')
+    # Convert R object to pandas df
+    data = m2ar(data)
+    return data
 
 
 
